9fb6b2ea8dd04af6fcac896893454cabee7e71a7
RafaelCMendes <rafaelcorreamendes@hotmail.com>
2020-08-08 11:06:01 -0300

    aula 5

---
+++ Aula 5/src/js/index.js
@@ -0,0 +1,88 @@
+import {Funcionarios} from "./Funcionarios.js"
+
+// var valor = prompt('digite um numero');
+// var numero  = parseInt(numero);
+
+// if(isNaN(numero)){
+//     alert('Você não digitou um numero - ' + valor);
+// } else if(numero%2 != 0){
+//     alert('Impar');
+// }else{
+//     alert('Par');
+// }
+
+// Resolver esse problema e explicar 
+// aonde esta o bug e porque estava ocorrendo. ;)
+
+// var txtValor1 = prompt('digite o número que será multiplicado');
+// var numero1 = parseInt(txtValor1);
+
+// var txtValor2 = prompt('digite até quanto deseja multiplicar');
+// var numero2 = parseInt(txtValor2);
+
+// for (var m = 0; m <= numero2; m++) {
+//     var resultado = numero1 * m;
+//     document.write(numero1 + ' x ' + m + ' = ' + resultado + '<br>');
+// }
+
+// var nome = 10;
+
+// function teste(){
+//     var nome = 9;
+//     document.write(nome + '<br>');
+//     document.write(this.nome + '<br>');
+// }
+
+// if(true){
+//     let  nome = 9;
+//     document.write(nome + '<br>');
+//     document.write(this.nome + '<br>');  
+// }
+
+// document.write(nome + '<br>');  
+
+
+
+// let listaSistemas = [];
+// listaSistemas.push('Smart Trade');
+// listaSistemas.push('QuickShop');
+// listaSistemas.push('Green');
+// listaSistemas.push('Park\'s'); 
+// listaSistemas.push('Invoice');
+
+
+// let listaFuncionarios = new Array();
+ let f = new Funcionarios('Rafael',27,'Java');
+// listaFuncionarios.push(f);
+// ////listaFuncionarios.push(new Funcionarios('Ingrid',27,'PHP')); 
+// //listaFuncionarios.push(new Funcionarios('Thiago',28,'Delphi'));
+// //listaFuncionarios.push(new Funcionarios('Marlon',25,'Delphi'));   
+
+// let endereco = new Endereco('Padre tintorio','Varzea', 'Teresopolis', 290, 'RJ');
+
+// let trade = new Trade(listaSistemas, listaFuncionarios, 4, 'Marlon', 6, endereco);
+
+// document.write('Trade' + '<br>');
+
+// document.write('<br>');
+// document.write('Sistemas:' + '<br>');
+// for(sistema of trade.listaSistema){
+//     document.write(sistema + '<br>'); 
+// }
+
+// document.write('<br>');
+// document.write('Funcionarios:' + '<br>');
+// for(funcionario of trade.listaFuncionarios){
+//     document.write('Nome: ' + funcionario.nome + '<br>'); 
+// }
+
+
+
+  
+
+
+
+
+
+
+
